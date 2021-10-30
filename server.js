8d37c40cfee06d9126842c2cfb33d0bf8f7161f9
Ramasamy Kasiviswanathan <ramasamykasiviswanathan@ramasamys-air.attlocal.net>
2021-10-30 00:23:41 -0400

    Added expressJS and compression module

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+const compression = require("compression");
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(compression());
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import engagement from "./engagment.m4v";
 import "./App.scss";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [eventdate, setEventDate] = useState(
+    new Date("2022-02-07T00:00:00.000+05:30")
+  );
   return (
     <section className="App">
       <header className="video-header">
@@ -13,7 +13,7 @@ function App() {
           <source src={engagement} type="video/mp4" />
         </video>
         <div className="viewport-header">
-          <h1>Big Day is on {Date("2022-02-07T00:00:00+0530")}</h1>
+          <h1>Big Day is on {eventdate.toLocaleString()}</h1>
         </div>
       </header>
     </section>
