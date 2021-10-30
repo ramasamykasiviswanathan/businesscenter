09a85d29a093579073bcfa0f5330f8c6c10e6472
Ramasamy Kasiviswanathan <ramasamykasiviswanathan@ramasamys-air.attlocal.net>
2021-10-30 00:23:41 -0400

    Added expressJS and compression module

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express(); // create express app
 const compression = require("compression");
+const host = "0.0.0.0";
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
 app.use(compression());
@@ -9,6 +11,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("server started on port 5000");
 });
